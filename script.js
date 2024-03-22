5d033ffd637b4b897f899d586119dbca30c53610
tiarahasnabila <tiarahasnabila@gmail.com>
2024-03-22 22:49:58 +0700

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+function beriSalam() {
+    alert('Selamat datang di Toko Kami !');
+  }
+  
+  function show(){
+    alert("Pesanan kamu sudah masuk! Mohon ditunggu ya")
+  }
+  
+  document.addEventListener('DOMContentLoaded', beriSalam);
